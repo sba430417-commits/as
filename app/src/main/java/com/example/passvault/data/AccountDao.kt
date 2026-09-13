cc0562877341d7bec855bda623dcdca20ceef679
sba430417-commits <sba430417@gmail.com>
2026-09-13 06:26:03 +0300

    Add project file app/src/main/java/com/example/passvault/data/AccountDao.kt

---
+++ app/src/main/java/com/example/passvault/data/AccountDao.kt
@@ -0,0 +1,29 @@
+package com.example.passvault.data
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface AccountDao {
+
+    @Query("SELECT * FROM accounts ORDER BY siteName COLLATE NOCASE ASC")
+    fun getAll(): LiveData<List<Account>>
+
+    @Query("SELECT * FROM accounts ORDER BY siteName COLLATE NOCASE ASC")
+    suspend fun getAllNow(): List<Account>
+
+    @Insert
+    suspend fun insert(account: Account): Long
+
+    @Insert
+    suspend fun insertAll(accounts: List<Account>)
+
+    @Update
+    suspend fun update(account: Account)
+
+    @Delete
+    suspend fun delete(account: Account)
+
+    @Query("SELECT * FROM accounts WHERE id = :id")
+    suspend fun getById(id: Long): Account?
+}
